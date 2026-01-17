8d8295aaabfa8155d6037c34e850058f77a33158
mj010504 <mj010504@naver.com>
2026-01-17 16:41:12 +0900

    [NDGL-12] chore: 불필요한 import 제거

--- core/ui/src/main/java/com/yapp/ui/base/BaseViewModel.kt
+++ core/ui/src/main/java/com/yapp/ui/base/BaseViewModel.kt
@@ -16,7 +16,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
 
--- feature/travel/src/main/java/com/yapp/ndgl/feature/travel/TravelViewModel.kt
+++ feature/travel/src/main/java/com/yapp/ndgl/feature/travel/TravelViewModel.kt
@@ -2,7 +2,6 @@ package com.yapp.ndgl.feature.travel
 
 import com.yapp.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 @HiltViewModel
