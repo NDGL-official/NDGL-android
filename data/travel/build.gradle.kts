c8c954e6e48ba2775407145768d4bdd68290119e
mj010504 <mj010504@naver.com>
2026-02-18 20:21:42 +0900

    [NDGL-66] chore: AddItinerary, AddPlace에 필요한 의존성, 아이콘, 문자열 리소스 추가

--- data/travel/build.gradle.kts
+++ data/travel/build.gradle.kts
@@ -1,11 +1,26 @@
+import java.util.Properties
+
 plugins {
     id("ndgl.data")
 }
 
 android {
     namespace = "com.yapp.ndgl.data.travel"
+
+    defaultConfig {
+        val localProperties = Properties().apply {
+            load(rootProject.file("local.properties").bufferedReader())
+        }
+        buildConfigField("String", "PLACE_API_KEY", "\"${localProperties.getProperty("PLACE_API_KEY", "")}\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
     implementation(project(":data:core"))
+    implementation(libs.places)
+    implementation(libs.kotlinx.coroutines.play.services)
 }
