7ddb9183a6971e3a2003aa298a451bd349983594
mj010504 <mj010504@naver.com>
2026-02-18 20:27:40 +0900

    [NDGL-66] feature: Google Place autoComplete API 연동

--- data/travel/src/main/java/com/yapp/ndgl/data/travel/di/TravelNetworkModule.kt
+++ data/travel/src/main/java/com/yapp/ndgl/data/travel/di/TravelNetworkModule.kt
@@ -1,5 +1,9 @@
 package com.yapp.ndgl.data.travel.di
 
+import android.content.Context
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.yapp.ndgl.data.travel.BuildConfig
 import com.yapp.ndgl.data.travel.api.PlaceApi
 import com.yapp.ndgl.data.travel.api.TravelProgramApi
 import com.yapp.ndgl.data.travel.api.TravelTemplateApi
@@ -7,6 +11,7 @@ import com.yapp.ndgl.data.travel.api.UserTravelApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -14,6 +19,17 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object TravelNetworkModule {
+    @Provides
+    @Singleton
+    fun providePlacesClient(
+        @ApplicationContext context: Context,
+    ): PlacesClient {
+        if (!Places.isInitialized()) {
+            Places.initializeWithNewPlacesApiEnabled(context, BuildConfig.PLACE_API_KEY)
+        }
+        return Places.createClient(context)
+    }
+
     @Provides
     @Singleton
     fun provideTravelProgramApi(
