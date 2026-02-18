abbbf276ee61c84a5f6776ee798adc33e6f24878
Jihee.Han <jihee.dev@gmail.com>
2026-02-18 23:15:39 +0900

    [NDGL-101] feat: 여행 템플릿 검색 API 구현

--- data/travel/src/main/java/com/yapp/ndgl/data/travel/repository/TravelTemplateRepository.kt
+++ data/travel/src/main/java/com/yapp/ndgl/data/travel/repository/TravelTemplateRepository.kt
@@ -4,6 +4,7 @@ import com.yapp.ndgl.data.core.model.getData
 import com.yapp.ndgl.data.travel.api.TravelTemplateApi
 import com.yapp.ndgl.data.travel.model.PopularTravelTemplates
 import com.yapp.ndgl.data.travel.model.RecommendTravelTemplates
+import com.yapp.ndgl.data.travel.model.SearchTravelTemplates
 import com.yapp.ndgl.data.travel.model.TravelTemplateContentInfo
 import com.yapp.ndgl.data.travel.model.TravelTemplateItinerary
 import javax.inject.Inject
@@ -18,7 +19,9 @@ class TravelTemplateRepository @Inject constructor(
     }
 
     suspend fun getPopularTravelTemplates(travelProgramId: Long): PopularTravelTemplates {
-        return travelTemplateApi.getPopularTravelTemplates(travelProgramId = travelProgramId).getData()
+        return travelTemplateApi.getPopularTravelTemplates(
+            travelProgramId = travelProgramId,
+        ).getData()
     }
 
     suspend fun getRecommendTravelTemplates(): RecommendTravelTemplates {
@@ -31,5 +34,9 @@ class TravelTemplateRepository @Inject constructor(
 
     suspend fun getTravelTemplateContentInfo(travelId: Long): TravelTemplateContentInfo {
         return travelTemplateApi.getTravelTemplateContentInfo(id = travelId).getData()
+    }
+
+    suspend fun searchTravelTemplates(keyword: String): SearchTravelTemplates {
+        return travelTemplateApi.searchTravelTemplates(keyword = keyword).getData()
     }
 }
