3b1ecd6619be880424bc30842bc3f5f46aa78300
mj010504 <mj010504@naver.com>
2026-05-15 22:02:52 +0900

    [NDGL-140] fix: TravelTheme 일부 Class명 수정

--- feature/content-recommendation/src/main/java/com/yapp/ndgl/feature/contentrecommendation/model/TravelTheme.kt
+++ feature/content-recommendation/src/main/java/com/yapp/ndgl/feature/contentrecommendation/model/TravelTheme.kt
@@ -2,11 +2,11 @@ package com.yapp.ndgl.feature.contentrecommendation.model
 
 enum class TravelTheme(val displayName: String) {
     FOOD("맛집"),
-    CAFE_DESSERT("카페/디저트"),
-    HEALING_SCENERY("힐링/풍경"),
-    LANDMARK("명소"),
+    CAFE("카페/디저트"),
+    HEALING("힐링/풍경"),
+    ATTRACTION("명소"),
     LOCAL("로컬"),
     SHOPPING("쇼핑"),
     ACTIVITY("액티비티/체험"),
-    VALUE_FOR_MONEY("가성비"),
+    BUDGET("가성비"),
 }
